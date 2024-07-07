82d56834d7ac9c8f6715045e615c16079f8b63d9
= <=>
2024-07-07 22:52:37 +0300

    güncelleme

--- src/MatrisMatrisCarp.java
+++ src/MatrisMatrisCarp.java
@@ -6,21 +6,23 @@ public class MatrisMatrisCarp {
         int[][] matrixB = new int[4][2];
         matrixB = new int[][]{{-1, 0}, {1, 2}, {3, 3}, {-1, 1}};
 
-
-        int[][] result = new int[matrixA.length][matrixB[0].length];
-        for (int i = 0; i < matrixA[0].length; i++) {
-            for (int row = 0; row < result.length; row++) {
-                for (int col = 0; col < result[row].length; col++) {
-                    result[row][col] += matrixA[row][i] * matrixB[i][col];
+        if(matrixA[0].length!=matrixB.length){
+            System.out.println("Bu matrisler çarpılamaz");
+        }else {
+            int[][] result = new int[matrixA.length][matrixB[0].length];
+            for (int i = 0; i < matrixA[0].length; i++) {
+                for (int row = 0; row < result.length; row++) {
+                    for (int col = 0; col < result[row].length; col++) {
+                        result[row][col] += matrixA[row][i] * matrixB[i][col];
+                    }
                 }
             }
-        }
-        for (int row = 0; row < result.length; row++) {
-            for (int col = 0; col < result[row].length; col++) {
-                System.out.print(result[row][col] + " ");
+            for (int row = 0; row < result.length; row++) {
+                for (int col = 0; col < result[row].length; col++) {
+                    System.out.print(result[row][col] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
-
         }
     }
 }
